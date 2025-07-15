392aeb8333b28e71fa1b31eb9a3b6f40c3398b79
'taemin0507' <'xoals28655@gmail.com'>
2025-07-15 17:36:56 +0900

    2025-07-15 chap02 수업

---
+++ chap02/src/sec01/exam04/VariableScopeExample.java
@@ -0,0 +1,15 @@
+package sec01.exam04;
+
+public class VariableScopeExample {
+
+	public static void main(String[] args) {
+		int v1 = 15; // main 메소드 블록에서 선언
+		
+		if(v1>10) {
+//			int v2; // if 블록에서 선언
+//			v2 = v1 - 10;
+		}
+//		int v3 = v1 + v2 +5; //유효범위를 벗어난 v2 변수를 사용할 수 없음
+	}
+
+}
---
+++ chap02/src/sec02/exam05/StringExample.java
@@ -0,0 +1,25 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// String: 참조타입, 타입이 대문자로 시작함에 주의
+		String name = "이태민";
+		String job = "프로그래머";
+		
+		System.out.println(name);
+		System.out.println(job);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
